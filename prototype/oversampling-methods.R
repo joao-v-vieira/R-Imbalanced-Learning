458ad2850797d4dbd7fa143c1d567f5d0917c4d3
plapa <lapadsk@hotmail.com>
2017-02-16 18:31:34 +0000

    a new boolean parameter that checks if to return the full dataset or only the generated data

--- prototype/oversampling-methods.R
+++ prototype/oversampling-methods.R
@@ -1,4 +1,4 @@
-random_oversampling <- function(input_matrix, num_samples = NULL){
+random_oversampling <- function(input_matrix, num_samples = NULL, only_generated_data = FALSE){
 
 if (is.null(num_samples)) {
  num_samples = length(subset(input_matrix[, ncol(input_matrix)],input_matrix[, ncol(input_matrix)] == 0 )) - length(subset(input_matrix[, ncol(input_matrix)],input_matrix[, ncol(input_matrix)] == 1 )) 
@@ -9,21 +9,27 @@ if (is.null(num_samples)) {
   #if mean > 0.5, 0 is minority
   #if mean < 0.5, 1 is minority
   
-  if (mean(input_matrix [, ncol (input_matrix)]) < 0.5){
-    minority_class <- 1
-  } else if (mean(input_matrix [, ncol(input_matrix)]) > 0.5){
-    minority_class <- 0
-  } else {
-    stop("Data set is perfectly balanced!")
-  }
+if (mean(input_matrix [, ncol (input_matrix)]) < 0.5){
+  minority_class <- 1
+} else if (mean(input_matrix [, ncol(input_matrix)]) > 0.5){
+  minority_class <- 0
+} else {
+  stop("Data set is perfectly balanced!")
+}
   
-  if (num_samples > 0){
-    #create num_samples rows with randomnly chosen rows which last variable has the minority class
-    target_rows <- which(input_matrix [, ncol(input_matrix)] == minority_class, arr.ind = TRUE)
-    random_rows <- sample(target_rows, num_samples, replace = TRUE)
-    output_matrix <- input_matrix[random_rows, ]
-  }
-  
-  #return the randomly copied dataset rows
+if (num_samples > 0){
+  #create num_samples rows with randomnly chosen rows which last variable has the minority class
+  target_rows <- which(input_matrix [, ncol(input_matrix)] == minority_class, arr.ind = TRUE)
+  random_rows <- sample(target_rows, num_samples, replace = TRUE)
+  output_matrix <- input_matrix[random_rows, ]
+}
+
+#return only the generated data or the full dataset, depending on the value parsed    
+if(only_generated_data){
   return (output_matrix)
+} else {
+  output_matrix <- rbind(input_matrix, output_matrix)
+  return(output_matrix)
+}
+  
 }
