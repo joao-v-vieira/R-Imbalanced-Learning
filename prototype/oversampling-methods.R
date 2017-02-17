bd791e35ba243ac7c4c48926c7d7db40b597d2f6
gdouzas <gdouzas@icloud.com>
2017-02-17 23:32:15 +0200

    Refactored check of binary target

--- prototype/oversampling-methods.R
+++ prototype/oversampling-methods.R
@@ -1,34 +1,29 @@
 random_oversampling <- function(input_matrix, num_samples = NULL, only_generated_data = TRUE) {
   
+  # Checks
+  target_column <- input_matrix[, ncol(input_matrix)]
+  target_column <- check_binary_target(target_column)
+  
   # Dataset parameters
-  num_columns <- ncol(input_matrix)
-  target_column <- input_matrix[, num_columns]
   minority_class_indices = which(target_column == 1)
   majority_class_indices = which(target_column == 0)
-  
-  if (length(unique(target_column)) == 2) {
     
-    # Create default value for num_samples
-    if (is.null(num_samples)) {
-      num_samples = length(majority_class_indices) - length(minority_class_indices)
-    }  
+  # Create default value for num_samples
+  if (is.null(num_samples)) {
+    num_samples = length(majority_class_indices) - length(minority_class_indices)
+  }  
     
-    # Generate data as exact copies of current minority class data
-    if (num_samples > 0) {
-      random_minority_indices <- sample(minority_class_indices, num_samples, replace = TRUE)
-      output_matrix <- input_matrix[random_minority_indices, ]
-    }
-    
-    # Append generated data to current data
-    if (!only_generated_data) {
-      output_matrix <- rbind(input_matrix, output_matrix)
-    }
-    
-    return(output_matrix)
-    
+  # Generate data as exact copies of current minority class data
+  if (num_samples > 0) {
+    random_minority_indices <- sample(minority_class_indices, num_samples, replace = TRUE)
+    output_matrix <- input_matrix[random_minority_indices, ]
   }
-  else {
-    message("Not a binary variable set as target")
+    
+  # Append generated data to current data
+  if (!only_generated_data) {
+    output_matrix <- rbind(input_matrix, output_matrix)
   }
+    
+  return(output_matrix)
   
 }
