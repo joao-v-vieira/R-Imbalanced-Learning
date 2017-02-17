1254aa159866bbf5a96aa331952f26033ed485c0
gdouzas <gdouzas@icloud.com>
2017-02-17 22:51:38 +0200

    Refactored code

--- prototype/oversampling-methods.R
+++ prototype/oversampling-methods.R
@@ -1,40 +1,45 @@
 random_oversampling <- function(input_matrix, num_samples = NULL, only_generated_data = TRUE) {
   
-  if (length(unique(input_matrix[,ncol(input_matrix)]))==2) {
+  # Dataset parameters
+  num_columns <- ncol(input_matrix)
+  target_column <- input_matrix[, num_columns]
+  minority_class_indices = which(target_column == 1)
+  majority_class_indices = which(target_column == 0)
+  
+  if (length(unique(target_column)) == 2) {
     
     # Create default value for num_samples
     if (is.null(num_samples)) {
-      num_samples = length(which(input_matrix[ , ncol(input_matrix)] == 0)) - length(which(input_matrix[ , ncol(input_matrix)] == 1))
-      }  
+      num_samples = length(majority_class_indices) - length(minority_class_indices)
+    }  
     
     # Check class distribution
-    if (length(which(input_matrix[ , ncol(input_matrix)] == 0)) - length(which(input_matrix[ , ncol(input_matrix)] == 1))){
+    if (length(minority_class_indices) - length(majority_class_indices)) {
       minority_class <- 1
-      } 
-    else if (length(which(input_matrix[ , ncol(input_matrix)] == 1)) - length(which(input_matrix[ , ncol(input_matrix)] == 0))) {
+    } 
+    else if (length(majority_class_indices) - length(minority_class_indices)) {
       minority_class <- 0
-      } 
+    } 
     else {
       stop("Data set is perfectly balanced!")
     }
     
     # Generate data as exact copies of current minority class data
     if (num_samples > 0) {
-      target_rows <- which(input_matrix [, ncol(input_matrix)] == minority_class, arr.ind = TRUE)
-      random_rows <- sample(target_rows, num_samples, replace = TRUE)
-      output_matrix <- input_matrix[random_rows, ]
-      }
-    
-    # Return generated data or append them to current data
-    if (only_generated_data) {
-      return (output_matrix)
-      } 
-    else {
-      output_matrix <- rbind(input_matrix, output_matrix)
-      return(output_matrix)
-      }
+      random_minority_indices <- sample(minority_class_indices, num_samples, replace = TRUE)
+      output_matrix <- input_matrix[random_minority_indices, ]
     }
+    
+    # Append generated data to current data
+    if (!only_generated_data) {
+      output_matrix <- rbind(input_matrix, output_matrix)
+    }
+    
+    return(output_matrix)
+    
+  }
   else {
     message("Not a binary variable set as target")
-    }
   }
+  
+}
