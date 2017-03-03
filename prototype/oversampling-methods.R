80af5607c70416d7819b380f1c27f088a17bdbde
joao-v-vieira <joaovieira1996@hotmail.com>
2017-03-03 18:40:25 +0000

    new function to check binary variable

--- prototype/oversampling-methods.R
+++ prototype/oversampling-methods.R
@@ -38,3 +38,22 @@ if(only_generated_data){
   }
   
 }
+check_binary<- function(input_matrix){
+  levels <-as.matrix(unique(input_matrix[,ncol(input_matrix)]))
+  first<- levels[1,1]
+  second<- levels[2,1]
+  minority <- subset(input_matrix, input_matrix[,5]==first)
+  majority <- subset(input_matrix, input_matrix[,5]==second)
+  
+  if(first != 0) {
+    for(first in minority[,5])
+      minority[,5]=0
+  }
+  
+  if (second != 1){
+    for(second in majority[,5])
+      majority[,5]=1
+  }
+    return(rbind(minority,majority))
+}   
+
