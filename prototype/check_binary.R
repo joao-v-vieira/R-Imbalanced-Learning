ffd324e0736693739b0d528f8fea929f70ad9aaf
joao-v-vieira <joaovieira1996@hotmail.com>
2017-03-03 18:49:13 +0000

    generalized number of columns

--- prototype/check_binary.R
+++ prototype/check_binary.R
@@ -2,17 +2,17 @@ check_binary<- function(input_matrix){
   levels <-as.matrix(unique(input_matrix[,ncol(input_matrix)]))
   first<- levels[1,1]
   second<- levels[2,1]
-  minority <- subset(input_matrix, input_matrix[,5]==first)
-  majority <- subset(input_matrix, input_matrix[,5]==second)
+  minority <- subset(input_matrix, input_matrix[,ncol(input_matrix)]==first)
+  majority <- subset(input_matrix, input_matrix[,ncol(input_matrix)]==second)
   
   if(first != 0) {
-    for(first in minority[,5])
-      minority[,5]=0
+    for(first in minority[,ncol(input_matrix)])
+      minority[,ncol(input_matrix)]=0
   }
   
   if (second != 1){
-    for(second in majority[,5])
-      majority[,5]=1
+    for(second in majority[,ncol(input_matrix)])
+      majority[,ncol(input_matrix)]=1
   }
   return(rbind(minority,majority))
 }   
